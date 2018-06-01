144f285ded0ea59508062a122c2fa9c8f6d56f5c
paul <paul.westenthanner@web.de>
2018-06-01 14:02:33 +0200

    setup.py script

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 
 """
-Setting up program for Apyori.
+Setting up program for TrainTicket Extractor.
 """
 
-import TrainTickets
+import pyDBtickets
 import setuptools
 
 setuptools.setup(
     name='pyDBtickets',
     description='Functionality to read train tickets issued by Deutsche Bahn (DB)',
     long_description=open('README.md').read(),
-    version=TrainTickets.__version__,
-    author=TrainTickets.__author__,
-    author_email=TrainTickets.__author_email__,
-    url='https://github.com/ymoch/apyori',
-    py_modules=['apyori'],
+    version=pyDBtickets.__version__,
+    author=pyDBtickets.__author__,
+    author_email=pyDBtickets.__author_email__,
+    url='https://github.com/PaulWestenthanner/pyDBtickets',
+
+    packages=[
+        'pyDBtickets',
+    ],
     test_suite='nose.collector',
     tests_require=['nose', 'mock'],
     entry_points={
@@ -24,19 +27,19 @@ setuptools.setup(
         ],
     },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Freelancers',
+        'Intended Audience :: Frequent Travellers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Tools :: Travel Expenses',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
